e8d2e23e20d82553e9d5873b5de83cfe3c9a53bd
hutterfly <106876855+hutterfly@users.noreply.github.com>
2022-07-23 20:49:21 -0400

    Update payloadhexmaker.py

--- payloadhexmaker.py
+++ payloadhexmaker.py
@@ -3,12 +3,11 @@ file = open("filedump.txt", "r")
 raw = file.read()
 no_space = raw.replace('\n', '')
 splitting = re.split("(.{2})", no_space)
-
-final = ""
-for x in range(len(splitting)):
-	if (x % 2) == 0 and x != (len(splitting)-1) and x != 0:
-		final = final + ","
-	else:
-		final = final + splitting[x]
-
+final = splitting[0]
+for x in range(len(splitting)-1):
+        x += 1
+        if (x % 2) == 0 and x != (len(splitting)-1):
+                final += ","
+        else:
+                final += splitting[x]
 print(final)
