4e4dde540156853a921d49aaac8ec9b3aae26076
hutterfly <106876855+hutterfly@users.noreply.github.com>
2022-08-13 10:56:48 -0400

    Update payloadhexmaker.py

--- payloadhexmaker.py
+++ payloadhexmaker.py
@@ -4,8 +4,7 @@ raw = file.read()
 no_space = raw.replace('\n', '')
 splitting = re.split("(.{2})", no_space)
 final = splitting[0]
-for x in range(len(splitting)-1):
-        x += 1
+for x in range(1, len(splitting)):
         if (x % 2) == 0 and x != (len(splitting)-1):
                 final += ","
         else:
